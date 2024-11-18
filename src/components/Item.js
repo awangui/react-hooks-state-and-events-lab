7ebec17374323ef88e42be663d8c0ad0634c663f
Anita Wangui <anitawangui22@gmail.com>
2024-11-18 10:23:59 +0300

    add use State hooks

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+
 function Item({ name, category }) {
+const[add , setAdd] = React.useState(false);
+const appClass = add ? "in-cart" : "";
+function addToCart(){
+  setAdd((add) => !add);
+}
+const buttonText = add ? "Remove from Cart" : "Add to Cart";
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={appClass} onClick={addToCart}>{buttonText}</button>
     </li>
   );
 }
